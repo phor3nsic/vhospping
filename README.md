d19d222ac51041d903ea3c8672dab7cefa5db849
phor3nsic <phorensic@protonmail>
2021-04-08 11:07:47 -0300

    banner in code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 _Abuse of vhost hopping or proxy pass_
 
 ## About
->
+> https://mobile.twitter.com/Bugcrowd/status/1372034980164014082/photo/1
 
 ## Usage
 > 
--- vhospping.py
+++ vhospping.py
@@ -7,10 +7,6 @@ from concurrent.futures import ThreadPoolExecutor
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-FIRST_RESPONSE = {}
-HOSTS_FOUND = []
-PROXY = None
-
 def getDomain(url):
 	r = '(?<=\\.)[\\w+.]+'
 	d = re.findall(r, url)
@@ -55,7 +51,7 @@ def outputSave(url, output):
 	arq = open(output, "w+")
 	for x in HOSTS_FOUND:
 		x = x.replace("\n","")
-		arq.write(f"[hosthopping] [host:{x}] [low] {url}\n")
+		arq.write(f"[vhospping] [host:{x}] [low] {url}\n")
 	arq.close()
 
 def run(host):
@@ -63,6 +59,7 @@ def run(host):
 
 def main():
 	getFirstReq(URL)
+	print(f"[!] Running on {URL}")
 
 	try:
 		with ThreadPoolExecutor() as t:
@@ -70,11 +67,22 @@ def main():
 	except KeyboardInterrupt:
 		outputSave(OUTPUT)
 		sys.exit()
-
-	outputSave(URL, OUTPUT)
+	
+	print(f"[!] Finish, total found {str(len(HOSTS_FOUND))}")
+	if len(HOSTS_FOUND) > 0:
+		print(f"[+] Se you results in {OUTPUT}")
+		outputSave(URL, OUTPUT)
 
 if __name__ == '__main__':
 	
+	banner = """\u001b[33;1m
+                   
+   /    _     '  _ 
+\//)()_) /)/)//)(/ 
+        / /    _/  
+
+	\u001b[0m"""
+	print(banner)
 	parser = argparse.ArgumentParser(add_help=True)
 	parser.add_argument("-u", "--url", help="Url",required=True)
 	parser.add_argument("-d", "--domain", help="Force Domain for header")
@@ -86,6 +94,9 @@ if __name__ == '__main__':
 
 	URL = args.url
 	OUTPUT = args.output
+	FIRST_RESPONSE = {}
+	HOSTS_FOUND = []
+	PROXY = None
 
 	if args.proxy:
 		PROXY = {"http":args.proxy,"https":args.proxy}
